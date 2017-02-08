a1b4415a754945d0078f7692aa8c2fe41127a31b
Abel968 <chenxuetao@qq.com>
2017-02-08 23:38:40 +0800

    the first window

--- mainwindow_mod.py
+++ mainwindow_mod.py
@@ -1,22 +1,46 @@
-import constants_mod
 
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui,QtCore,Qt
+from PyQt4.QtGui import *
 
-class Center(QtGui.QWidget):
+Window_Width = Window_Height = 600
+
+class GridLayout(QtGui.QWidget):
+    global Window_Height,Window_Width
+
     def __init__(self, parent = None):
+        global map
         QtGui.QWidget.__init__(self, parent)
         self.setWindowTitle('SnakeVersus')
-        self.resize(constants_mod.Window_Wideth, constants_mod.Window_Height)
+        self.resize(Window_Width, Window_Height)
         self.setWindowIcon(QtGui.QIcon('./Image/snake_icon.png'))
-        self.center()
 
-    def center(self):
-        screen = QtGui.QDesktopWidget().screenGeometry()
-        size = self.geometry()
-        self.move((screen.width() - size.width()) / 2,(screen.height() - size.height()) / 2)
+        map=[]
+        for i in range(400):
+            map.append(0)
 
-snake_app = QtGui.QApplication(sys.argv)
-main_window = Center()
-main_window.show()
-sys.exit(snake_app.exec_())
+        pe1 = QtGui.QPalette()
+        pe1.setColor(self.backgroundRole(), QColor(192, 253, 123)) #configure the color of the Empty box
+        pe2 = QtGui.QPalette()
+        pe2.setColor(self.backgroundRole(), QColor(192, 0, 123))  # configure the color of the Not-Empty box
+        grid = QtGui.QGridLayout()
+
+        for i in range(20):
+            for j in range(20):
+                label = QtGui.QLabel()
+                label.setAutoFillBackground(True)
+                if(map[i*20+j] == 0):
+                    label.setPalette(pe1)
+                else:
+                    label.setPalette(pe2)
+                label.setPalette(pe1)
+
+                grid.addWidget(label, i, j)
+
+        grid.setSpacing(1)
+        self.setLayout(grid)
+
+app = QtGui.QApplication(sys.argv)
+gridlayout = GridLayout()
+gridlayout.show()
+sys.exit(app.exec_())
